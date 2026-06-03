2aa2b20bd5b933ec76ed5d737c6234cb6693e37f
Joseph Fitzpatrick <joeyf116@yahoo.com>
2026-06-03 10:46:54 -0400

    Update prisma versioning

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,20 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
 	var __prisma: PrismaClient | undefined;
 }
 
+const connectionString =
+	process.env.DATABASE_URL ||
+	"postgresql://postgres:postgres@localhost:5432/postgres";
+
+const adapter = new PrismaPg({ connectionString });
+
 export const prisma =
 	global.__prisma ||
 	new PrismaClient({
+		adapter,
 		log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
 	});
 
